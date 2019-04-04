c67f7f4e7b70a1e63b2b528f0261eba2b94645d8
JKawmi <yousef.kawmi@gmail.com>
2019-04-04 16:07:41 +0200

    First version

---
+++ script.js
@@ -0,0 +1,17 @@
+function startTime() {
+	var today = new Date();
+	var h = today.getHours();
+	var m = today.getMinutes();
+	m = checkTime(m);
+	document.getElementById('datetime').innerHTML = h + ":" + m;
+	t = setTimeout('startTime()', 500);
+}
+
+function checkTime(i) {
+	if (i < 10) {
+		i = "0" + i;
+	}
+	return i;
+}
+
+startTime();
